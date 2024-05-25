0d3f836fc152708ed31a41962735fc97fffc9440
Parzivak <bherrera@unicauca.edu.co>
2024-05-25 17:07:56 -0500

    avance

--- proyectoGINA/src/views/componentHorario.vue
+++ proyectoGINA/src/views/componentHorario.vue
@@ -8,7 +8,7 @@
       <div class="row" v-for="(hour, index) in hours" :key="index">
         <div class="cell">{{ hour }}</div>
         <div class="cell" v-for="day in days" :key="day + index"
-          :class="{ selected: isSelected(day, hour), occupied: isOccupied(day, hour) }"
+          :class="cellClasses(day, hour)"
           @click="toggleSelection(day, hour)"></div>
       </div>
     </div>
@@ -39,9 +39,10 @@ export default {
   data() {
     return {
       days: ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'],
-      hours: this.generateHours(7, 22),
+      hours: this.generateHours(),
       selectedCells: [],
       horariosOcupados: [],
+      horariosOcupados2: []
     };
   },
   watch: {
@@ -56,7 +57,6 @@ export default {
     idDocente: {
       handler(newVal) {
         if (newVal) {
-          console.log(newVal)
           this.obtenerHorariosOcupadosDocente(newVal);
         }
       },
@@ -65,17 +65,18 @@ export default {
   },
   methods: {
     countWeeklyHours() {
-      const weeklyHours = this.selectedCells.length * 2; // Multiplicar por 2 ya que cada celda representa 2 horas
+      const occupiedCells = [...this.selectedCells, ...this.getOccupiedCells()];
+      const weeklyHours = occupiedCells.length * 2; // Multiplicar por 2 ya que cada celda representa 2 horas
       return weeklyHours;
     },
     countDailyHours() {
+      const occupiedCells = [...this.selectedCells, ...this.getOccupiedCells()];
       const dailyHours = {}; // Objeto para almacenar las horas diarias
-      this.selectedCells.forEach(cell => {
+      occupiedCells.forEach(cell => {
         if (!dailyHours[cell.day]) {
           dailyHours[cell.day] = 0; // Inicializar las horas diarias para cada día en 0
         }
-        alert(dailyHours[cell.day])
-        dailyHours[cell.day] += 1; // Sumar 2 horas por cada celda seleccionada
+        dailyHours[cell.day] += 2; // Sumar 2 horas por cada celda seleccionada
       });
       return dailyHours;
     },
@@ -91,17 +92,39 @@ export default {
       }
       return false;
     },
-    generateHours(start, end) {
-      const hours = [];
-      for (let i = start; i <= end; i++) {
-        let period = i < 12 ? 'AM' : 'PM';
-        let displayHour = i % 12 === 0 ? 12 : i % 12;
-        hours.push(`${displayHour} ${period}`);
-      }
-      return hours;
+    getOccupiedCells() {
+      const occupiedCells = [];
+      this.horariosOcupados.forEach(horario => {
+        occupiedCells.push({ day: horario.dia, hour: this.formatHour(horario.hora_inicio, horario.hora_fin) });
+      });
+      this.horariosOcupados2.forEach(horario => {
+        occupiedCells.push({ day: horario.dia, hour: this.formatHour(horario.hora_inicio, horario.hora_fin) });
+      });
+      return occupiedCells;
+    },
+    formatHour(startHour, endHour) {
+      return `${startHour.split(':')[0]} ${startHour.split(' ')[1]} - ${endHour.split(':')[0]} ${endHour.split(' ')[1]}`;
+    },
+    generateHours() {
+      return [
+        '7 AM - 9 AM',
+        '9 AM - 11 AM',
+        '11 AM - 1 PM',
+        '2 PM - 4 PM',
+        '4 PM - 6 PM',
+        '6 PM - 8 PM',
+        '8 PM - 10 PM'
+      ];
     },
     isSelected(day, hour) {
       return this.selectedCells.some(cell => cell.day === day && cell.hour === hour);
+    },
+    cellClasses(day, hour) {
+      return {
+        selected: this.isSelected(day, hour),
+        'occupied-red': this.isOccupied(day, hour) === 'firstList',
+        'occupied-brown': this.isOccupied(day, hour) === 'secondList'
+      };
     },
     isOccupied(day, hour) {
       // Verifica si el horario está ocupado en la primera lista
@@ -109,18 +132,22 @@ export default {
         horario.dia === day && this.isBetween(hour, horario.hora_inicio, horario.hora_fin)
       );
 
+      if (isOccupiedInFirstList) return 'firstList';
+
       // Verifica si el horario está ocupado en la segunda lista
       const isOccupiedInSecondList = this.horariosOcupados2.some(horario =>
         horario.dia === day && this.isBetween(hour, horario.hora_inicio, horario.hora_fin)
       );
 
-      // Retorna true si el horario está ocupado en alguna de las dos listas
-      return isOccupiedInFirstList || isOccupiedInSecondList;
-    },
+      if (isOccupiedInSecondList) return 'secondList';
 
+      // Retorna null si el horario no está ocupado en ninguna de las dos listas
+      return null;
+    },
     isBetween(hour, startHour, endHour) {
       const hourToNumber = (h) => {
-        const [time, period] = h.split(' ');
+        const [timeRange] = h.split(' - ');
+        const [time, period] = timeRange.split(' ');
         let [hours, minutes] = time.split(':').map(Number);
         if (period === 'PM' && hours !== 12) hours += 12;
         if (period === 'AM' && hours === 12) hours = 0;
@@ -129,57 +156,32 @@ export default {
       return hourToNumber(hour) >= hourToNumber(startHour) && hourToNumber(hour) < hourToNumber(endHour);
     },
     toggleSelection(day, hour) {
-  // Verificar si la celda está ocupada
-  if (!this.isOccupied(day, hour)) {
-    // Verificar si ya se ha alcanzado el límite de 3 bloques de dos horas
-    if (this.selectedCells.length >= 6) {
-      return; // No permitir más selecciones si se alcanzó el límite
-    }
-
-    // Verificar si la celda ya está seleccionada
-    const cellIndex = this.selectedCells.findIndex(cell => cell.day === day && cell.hour === hour);
-    if (cellIndex >= 0) {
-      // Si la celda ya está seleccionada, deseleccionarla
-      this.selectedCells.splice(cellIndex, 1);
-
-      // También deseleccionar la celda adyacente en la siguiente hora si está seleccionada
-      const nextHourIndex = this.hours.findIndex(h => h === hour) + 1;
-      if (nextHourIndex < this.hours.length) {
-        const nextHour = this.hours[nextHourIndex];
-        const nextHourCellIndex = this.selectedCells.findIndex(cell => cell.day === day && cell.hour === nextHour);
-        if (nextHourCellIndex >= 0) {
-          this.selectedCells.splice(nextHourCellIndex, 1);
+      // Verificar si la celda está ocupada
+      if (!this.isOccupied(day, hour)) {
+        // Verificar si ya se ha alcanzado el límite de 3 bloques de dos horas
+        if (this.selectedCells.length >= 6) {
+          return; // No permitir más selecciones si se alcanzó el límite
         }
-      }
-    } else {
-      // Si no está seleccionada, agregarla a las selecciones
-      this.selectedCells.push({ day, hour });
 
-      // Verificar si hay una celda adyacente en la siguiente hora y seleccionarla automáticamente
-      const nextHourIndex = this.hours.findIndex(h => h === hour) + 1;
-      if (nextHourIndex < this.hours.length) {
-        const nextHour = this.hours[nextHourIndex];
-        if (!this.isOccupied(day, nextHour)) {
-          // Verificar si ya se han seleccionado dos bloques de dos horas consecutivos
-          if (this.selectedCells.length % 2 === 0) {
-            // Si se han seleccionado dos bloques consecutivos, no permitir más selecciones
-            return;
-          }
-          this.selectedCells.push({ day, hour: nextHour });
+        // Verificar si la celda ya está seleccionada
+        const cellIndex = this.selectedCells.findIndex(cell => cell.day === day && cell.hour === hour);
+        if (cellIndex >= 0) {
+          // Si la celda ya está seleccionada, deseleccionarla
+          this.selectedCells.splice(cellIndex, 1);
+        } else {
+          // Si no está seleccionada, agregarla a las selecciones
+          this.selectedCells.push({ day, hour });
+        }
+
+        // Verificar si se exceden las horas diarias o semanales
+        if (this.isDailyHoursExceeded()) {
+          alert("Se han excedido las horas diarias permitidas.");
+        }
+        if (this.isWeeklyHoursExceeded()) {
+          alert("Se han excedido las horas semanales permitidas.");
         }
       }
     }
-    if (this.isDailyHoursExceeded()) {
-        alert("Se han excedido las horas diarias permitidas.");
-      }
-      if (this.isWeeklyHoursExceeded()) {
-        alert("Se han excedido las horas semanales permitidas.");
-      }
-  }
-}
-
-
-
   }
 };
 </script>
@@ -223,8 +225,14 @@ export default {
   color: white;
 }
 
-.cell.occupied {
+.cell.occupied-red {
   background-color: #ff0000;
+  pointer-events: none;
+  /* Deshabilita eventos de clic en celdas ocupadas */
+}
+
+.cell.occupied-brown {
+  background-color: #8b4513;
   pointer-events: none;
   /* Deshabilita eventos de clic en celdas ocupadas */
 }
